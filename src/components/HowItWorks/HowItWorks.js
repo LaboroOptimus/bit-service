16dffbf8b97d8c4ea2370bc16106ced8a7f571eb
Igor <igor.salesmarketing@gmail.com>
2019-10-30 17:26:46 +0300

    Добавил время в блок "Как это работает"

--- src/components/HowItWorks/HowItWorks.js
+++ src/components/HowItWorks/HowItWorks.js
@@ -12,7 +12,7 @@ const Wrapper = styled.div`
 const HowItWorksContainer = styled.div`
     display: flex;
     flex-direction: column;
-    width: 30%;
+    width: 35%;
 `;
 
 const HowItWorksTitle = styled.h3`
@@ -21,12 +21,21 @@ const HowItWorksTitle = styled.h3`
 
 const HowItWorksContent = styled.p`
     font-size: 17px;
+    padding-right: 60px;
 `
 const Title = styled.h2`
     text-align: center;
     font-size: 40px;
     margin-top: 30px;
     margin-bottom: 30px;
+`;
+const Time = styled.span`
+    font-size: 15px;
+    color: #fff;
+    background-color:#ff3a00;
+    border-radius: 20px;
+    font-weight: normal;
+    padding: 3px 5px;
 `;
 
 const HowItWorks = () => {
@@ -36,20 +45,20 @@ const HowItWorks = () => {
         <Wrapper>
             <HowItWorksContainer>
                 <div>
-                    <HowItWorksTitle>Создаете заявку</HowItWorksTitle>
+                    <HowItWorksTitle>Создаете заявку <Time>~ 3 минуты</Time></HowItWorksTitle>
                     <HowItWorksContent>с кратким описанием работ и желаемой датой ремонта. Потратите не более 3 минут</HowItWorksContent>
                 </div>
                 <div>
-                    <HowItWorksTitle>Получаете отклики</HowItWorksTitle>
+                    <HowItWorksTitle>Получаете отклики <Time>~ 15 минут</Time></HowItWorksTitle>
                     <HowItWorksContent>от специализированных сервисов по SMS или в личном кабинете</HowItWorksContent>
                 </div>
             </HowItWorksContainer>
             <HowItWorksContainer>
                 <div>
-                    <HowItWorksTitle>Сравниваете ответы</HowItWorksTitle>
+                    <HowItWorksTitle>Сравниваете ответы <Time>~ 10 минут</Time></HowItWorksTitle>
                     <HowItWorksContent>наиболее подходящие по стоимости, отзывам, местоположению и другим параметрам</HowItWorksContent></div>
                 <div>
-                    <HowItWorksTitle>Подтверждаете запись</HowItWorksTitle>
+                    <HowItWorksTitle>Подтверждаете запись <Time>~ 1 минута</Time></HowItWorksTitle>
                     <HowItWorksContent>а также все условия ремонта и можно смело ехать в автосервис</HowItWorksContent>
                 </div>
             </HowItWorksContainer>
