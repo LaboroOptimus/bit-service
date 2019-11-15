ddf7eae2ce9d602e905c89a8d927f08e72776983
Igor <igor.salesmarketing@gmail.com>
2019-11-15 22:17:17 +0300

    Обновил компонент Login

--- src/components/Navtop/Navtop.js
+++ src/components/Navtop/Navtop.js
@@ -5,35 +5,44 @@ import {NavLink} from 'react-router-dom'
 const NavPanel = styled.div`
     display: flex;
     flex-direction: row;
-    justify-content: space-around;
-    margin-left: 40px;
+    justify-content: space-between;
+    position: fixed;
+    width: 100%;
+    background: #fff;
+    height: 50px;
+    box-shadow: 0px 4px 4px rgba(201, 200, 200, 0.15)
 `;
 
 const Phone = styled.h4`
-    font-size: 16px;
+    font-size: 15px;
     margin: 0;
-    flex-grow:1;
     margin-top: 10px;
     margin-bottom: 10px;
-    margin-left: 30px;
+    font-weight: 400;
+    margin-left: 60px;
     
 `;
 
 const NavBlock = styled.div`
-    padding-left: 5px;
-    padding-right: 5px;
     margin-top: 10px;
     margin-bottom: 10px;
+    margin-right: 30px;
 `;
+
+const Link = styled(NavLink)`
+    margin-right: 5px;
+
+`
 
 
 const Navtop = () => {
     return (
         <NavPanel>
-            <Phone>8(800)000-00-00</Phone>
-            <NavBlock><NavLink to="/for-service">Вы владелец сервиса?</NavLink></NavBlock>
-            <NavBlock><NavLink to="/map">Найти сервис на карте</NavLink></NavBlock>
-            <NavBlock><NavLink to="/login">Войти на сайт</NavLink></NavBlock>
+            <Phone>Меню</Phone>
+            <NavBlock><Link to="/for-service">Регистрация</Link>
+                <Link to="/login">Вход</Link>
+            </NavBlock>
+           {/* <NavBlock><NavLink to="/login">Вход</NavLink></NavBlock>*/}
         </NavPanel>
     )
 }
