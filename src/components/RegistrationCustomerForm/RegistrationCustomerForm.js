7faacdc329568450b78c77f6177ef78ea899a69d
Igor <igor.salesmarketing@gmail.com>
2019-11-22 18:31:13 +0300

    Рефакторинг redux

--- src/components/RegistrationCustomerForm/RegistrationCustomerForm.js
+++ src/components/RegistrationCustomerForm/RegistrationCustomerForm.js
@@ -2,8 +2,8 @@ import React from 'react'
 import styled from "styled-components";
 import {NavLink} from "react-router-dom";
 import {connect} from "react-redux";
-import axios from 'axios'
-import fire from "../../config/Fire";
+import {changeName, changeCity,changeEmail,changePass,changePhone,changeStation,submit} from '../../redux/action-creators.js'
+
 
 
 const Wrapper = styled.div`
@@ -164,196 +164,65 @@ const Checkbox = styled.input`
    }
 `;
 
-class RegistrationCustomerForm extends React.Component {
-/*    state = {
-        name:'',
-        email: '',
-        phone: '',
-        city: '',
-        passOne: '',
-        agree: false,
-        uid:'',
-        showPassTips: false,
-        showEmailTips: false,
-        showAgreeTips: false,
-        emailValidate: false,
-        passValidate: false,
-        isPassEquals: true,
-    };
-
-   onChangeAgree = () => {
-        this.setState({
-            agree: !this.state.agree,
-        })
-    }
-   onNameChanged = (e) => {
-       this.setState({
-           name: e.target.value
-       })
-   }
-   onPhoneChanged = (e) => {
-        this.setState({
-            phone: e.target.value
-        })
-    };
-   onCityChanged = (e) => {
-        this.setState({
-            city: e.target.value
-        })
-    };*/
- /*  onEmailChanged = (e) => {
-       this.setState({
-           email: e.target.value
-       })
-       console.log(this.state.email);
-       const reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-       if(reg.test(e.target.value) === true){
-           this.setState({
-               emailValidate: true,
-           })}
-       else {
-           this.setState({
-               emailValidate: false,
-           })
-           }
-       }*/
-/*   onPassChanged = (e) => {
-       this.setState({
-           passOne: e.target.value
-       })
-
-       const reg  = /^[a-zA-Z0-9!@#$%^&*]{6,16}$/;
-
-       if(reg.test(e.target.value)=== true){
-           this.setState({
-               passValidate: true,
-           })
-       }
-       else{
-           this.setState({
-               passValidate: false,
-           })
-       }
-   };*/
-
- /*  onSubmit = (e) => {
-       e.preventDefault();
-
-       if(this.state.passValidate && this.state.emailValidate && this.state.agree) {
-           fire.auth().createUserWithEmailAndPassword(this.state.email, this.state.passOne)
-               .then((response) => {
-                   let uid = response.user.uid;
-                   const data = {
-                       name: this.state.name,
-                       email: this.state.email,
-                       phone: this.state.phone,
-                       city: this.state.city,
-                   }
-                   axios.post(`https://bit-ser.firebaseio.com/users/${uid}.json`, data);
-               }).then(() => {
-               this.setState({
-                   name:'',
-                   email: '',
-                   phone: '',
-                   city: '',
-                   passOne: '',
-                   agree: false,
-                   uid:'',
-                   showPassTips: false,
-                   showEmailTips: false,
-                   showAgreeTips: false,
-                   emailValidate: false,
-                   passValidate: false,
-                   isPassEquals: true,
-               })
-           })
-               .catch((e) => {
-                   console.log(e)
-               });
-       }
-       else if(!this.state.emailValidate){
-           this.setState({
-               showEmailTips: true,
-           })
-       }
-       else if(!this.state.passValidate){
-           this.setState({
-               showPassTips: true,
-           })
-       }
-       else if (!this.state.agree){
-           this.setState({
-               showAgreeTips: true
-           })
-       }
-       else{
-           console.log('введите данные верно')
-       }
-   }*/
-
-
-
-
-    render(){
-        const {name, city, station, phone,pass,email} = this.props;
-        console.log(name,city,station,phone,pass,email);
-        return(
-            <Wrapper>
-                <FormWrapper>
-                    <Title>Регистрация нового клиента</Title>
-                    <Row>
-                        <Container>
-                            <Label>Введите имя и фамилию</Label>
-                            <Input value={name} onChange={this.props.handleChangeName} placeholder={'Иван Иванов'}/>
-                        </Container>
-                        <Container>
-                            <Label>Введите телефон</Label>
-                            <Input value={phone} onChange={this.props.handleChangePhone} placeholder={'+7(800)000-00-00'}/>
-                        </Container>
-                    </Row>
-                    <Row>
-                        <Container>
-                            <Label>Введите город</Label>
-                            <Input value={city} onChange={this.props.handleChangeCity} placeholder={'Москва'}/>
-                        </Container>
-                        <Container>
-                            <Label>Введите ближайшее метро</Label>
-                            <Input value={station} onChange={this.props.handleChangeStation} placeholder={'Планерная'}/>
-                        </Container>
-                    </Row>
-                    <Row>
-                        <Container>
-                            <Label>Введите email</Label>
-                            <Input value={email} onChange={this.props.handleChangeEmail} placeholder={'service@mail.ru'}/>
-                        </Container>
-                        <Container>
-                            <Label>Введите пароль</Label>
-                            <Input value={pass} onChange={this.props.handleChangePass} type='password'/>
-                        </Container>
-                    </Row>
-                    <Row>
-                        <Container>
-                            <CheckboxGroup>
-                                <Checkbox id='phone_email' type='checkbox'/>
-                                <CheckboxLabel htmlFor={'phone_email'}>Согласен на обработку персональных данных</CheckboxLabel>
-                            </CheckboxGroup>
-                        </Container>
-                        <Container>
-                            <Links>
-                                <LinkDecription>Уже зарегистрированы?</LinkDecription>
-                                <Link to={'/login'}>Войти на сайт</Link>
-                            </Links>
-                        </Container>
-                    </Row>
-                    <Row>
-                        <Button onClick={this.props.submitHandler}
-                        >Зарегистрироваться</Button>
-                    </Row>
-                </FormWrapper>
-                {/*<button onClick={this.testFunc}>тест</button>*/}
-            </Wrapper>
-        )
-    }
+function RegistrationCustomerForm(props) {
+    const {name, city, station, phone, pass, email} = props;
+    return (
+        <Wrapper>
+            <FormWrapper>
+                <Title>Регистрация нового клиента</Title>
+                <Row>
+                    <Container>
+                        <Label>Введите имя и фамилию</Label>
+                        <Input value={name} onChange={props.onChangeName} placeholder={'Иван Иванов'}/>
+                    </Container>
+                    <Container>
+                        <Label>Введите телефон</Label>
+                        <Input value={phone} onChange={props.handleChangePhone} placeholder={'+7(800)000-00-00'}/>
+                    </Container>
+                </Row>
+                <Row>
+                    <Container>
+                        <Label>Введите город</Label>
+                        <Input value={city} onChange={props.handleChangeCity} placeholder={'Москва'}/>
+                    </Container>
+                    <Container>
+                        <Label>Введите ближайшее метро</Label>
+                        <Input value={station} onChange={props.handleChangeStation} placeholder={'Планерная'}/>
+                    </Container>
+                </Row>
+                <Row>
+                    <Container>
+                        <Label>Введите email</Label>
+                        <Input value={email} onChange={props.handleChangeEmail} placeholder={'service@mail.ru'}/>
+                    </Container>
+                    <Container>
+                        <Label>Введите пароль</Label>
+                        <Input value={pass} onChange={props.handleChangePass} type='password'/>
+                    </Container>
+                </Row>
+                <Row>
+                    <Container>
+                        <CheckboxGroup>
+                            <Checkbox id='phone_email' type='checkbox'/>
+                            <CheckboxLabel htmlFor={'phone_email'}>Согласен на обработку персональных
+                                данных</CheckboxLabel>
+                        </CheckboxGroup>
+                    </Container>
+                    <Container>
+                        <Links>
+                            <LinkDecription>Уже зарегистрированы?</LinkDecription>
+                            <Link to={'/login'}>Войти на сайт</Link>
+                        </Links>
+                    </Container>
+                </Row>
+                <Row>
+                    <Button onClick={() => props.submitHandler(name,city,email,pass,station, phone)}
+                    >Зарегистрироваться</Button>
+                </Row>
+            </FormWrapper>
+            {/*<button onClick={this.testFunc}>тест</button>*/}
+        </Wrapper>
+    )
 }
 
 const mapStateToProps = (state) => {
@@ -369,13 +238,13 @@ const mapStateToProps = (state) => {
 };
 
 const mapDispatchToProps = dispatch => ({
-    submitHandler: (e) => {dispatch({type:'SUBMIT'})},
-    handleChangeName:(e) => dispatch({type: "CHANGE_NAME", payload: e.target.value}),
-    handleChangeCity:(e) => dispatch({type: "CHANGE_CITY", payload: e.target.value}),
-    handleChangeEmail:(e) => dispatch({type: "CHANGE_EMAIL", payload: e.target.value}),
-    handleChangePass:(e) => dispatch({type: "CHANGE_PASS", payload: e.target.value}),
-    handleChangeStation:(e) => dispatch({type: "CHANGE_STATION", payload: e.target.value}),
-    handleChangePhone:(e) => dispatch({type: "CHANGE_PHONE", payload: e.target.value})
+    submitHandler: (name,city,email,pass,station, phone) => {dispatch(submit(name,city,email,pass,station, phone))},
+    onChangeName:(e) => dispatch(changeName(e)),
+    handleChangeCity:(e) => dispatch(changeCity(e)),
+    handleChangeEmail:(e) => dispatch(changeEmail(e)),
+    handleChangePass:(e) => dispatch(changePass(e)),
+    handleChangeStation:(e) => dispatch(changeStation(e)),
+    handleChangePhone:(e) => dispatch(changePhone(e))
 });
 
 
