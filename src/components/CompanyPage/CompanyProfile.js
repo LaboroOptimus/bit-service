ab81419b7c63225bedba85668599b15ed9e6122c
Igor <igor.salesmarketing@gmail.com>
2020-02-01 00:02:45 +0300

    Подключил шрифт и причесал компонент профиля компании

--- src/components/CompanyPage/CompanyProfile.js
+++ src/components/CompanyPage/CompanyProfile.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import styled from "styled-components";
-import {connect} from 'react-redux'
+import {connect} from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPen, faPencilAlt, faPenAlt} from '@fortawesome/free-solid-svg-icons'
+import clipboard from './assets/clipboard.png'
+import star from './assets/favorites.png';
+import review from './assets/review.png';
+import phone from './assets/phone.png'
 
 const Wrapper = styled.div`
     padding: 100px;
@@ -9,21 +15,33 @@ const Wrapper = styled.div`
 `;
 
 const Title = styled.h3`
-    font-size: 30px;
+    font-size: 40px;
     padding: 0;
     margin: 0 0 20px 0;
+    font-family: 'NotoSans-Bold';
+    color: #222;
+`;
+
+const MainProfile = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin: 20px;
 `;
 
 const SubTitle = styled.h4`
     font-size: 25px;
     padding: 0;
     margin: 0 0 20px 0px;
+    font-family: 'NotoSans-Bold';
+    color: #222;
 `;
 
 const PersonTitle = styled.h4`
     font-size: 25px;
     padding: 0;
-    margin: 0 0 20px 15px;
+    margin: 10px 0;
+    font-family: 'NotoSans-Bold';
+    color: #222;
 `;
 
 const Row = styled.div`
@@ -35,19 +53,19 @@ const Row = styled.div`
 const Column = styled.div`
     display: flex;
     flex-direction: column;
+    margin: ${props=> props.margin}
 `;
 
 const Img = styled.img`
-    height: 200px;
-    width: 200px;
-    
+    height: 225px;
+    width: 225px;
+    border-radius: 50%;
 `;
 
 const PersonInfoBlock = styled.div`
     display: flex;
     flex-direction: column;
-    margin-left: 15px;
-    margin-right: 15px;
+    margin-right: 25px;
 `;
 
 const CompanyInfoBlock = styled.div`
@@ -57,14 +75,16 @@ const CompanyInfoBlock = styled.div`
 `;
 
 const PersonLabel = styled.span`
-    font-size: 16px;
+    font-size: 14px;
     color: #A5A5A5;
-    margin-top: 10px;
+    margin-top: 15px;
+    font-family: 'NotoSans-Regular';
 `;
 
 const PersonInfo = styled.span`
     font-size: 20px;
-    font-weight: bold;
+    font-family: 'NotoSans-Bold';
+    color: #222;
 `;
 
 const Button = styled.button`
@@ -93,7 +113,10 @@ const Button = styled.button`
 `;
 
 const CompanyInfo = styled.span`
-    font-size: 18px;
+    font-size: 22px;
+    margin-bottom: 20px;
+    font-family: 'NotoSans-Bold';
+    color: #222;
 `;
 
 const Status = styled.span`
@@ -102,8 +125,9 @@ const Status = styled.span`
     background: #F07554;
     border-radius: 50px;
     margin-left: 20px;
-    margin-top: 10px;
     font-size: 14px;
+    font-family: 'NotoSans-Regular';
+    font-weight: 500;
 `;
 
 const Price = styled.div`
@@ -114,23 +138,139 @@ const Price = styled.div`
     width: 300px;
     background-color: ${props=>props.backgroundColor}  
 `;
+
+const EditLink = styled.a`
+    margin-top: 40px;
+    font-size: 21px;
+    color: #368594;
+    border-bottom: 2px dashed transparent;
+    
+    :hover {
+        cursor: pointer;
+        border-bottom: 2px solid #368594;
+    }
+`;
+
+const EditIcon = styled(FontAwesomeIcon)`
+    color: #368594;
+    font-size: 17px;
+    margin-right: 5px;
+  
+    :hover {
+        cursor: pointer;
+    }
+`;
+
+const CompanyLabel = styled.span`
+    font-size: 14px;
+    color: #A5A5A5
+`;
+
 const PriceName = styled.span`
- 
+    color: #222;
 `;
 const PriceValue = styled.span`
-   
+   color: #222;
 `;
+
+const AdnvantagesBlock = styled.div`
+    background-image: url(${props=>props.backgroundImage});
+    background-repeat: no-repeat;
+    background-position: center center;
+    padding: 35px;
+`;
+
+const AdnvantagesLabel = styled.p`
+    color: #444444;
+    text-align: center;
+    padding: 0;
+    margin: 10px 0 0 0;
+`;
+
+const AdvantagesCount = styled.h4`
+    color: #222;
+    text-align: center;
+    font-size: 60px;
+    font-family: 'NotoSans-Bold';
+    margin: 0 0 10px 0;
+    padding: 0;
+`;
+
+const AdditionItem = styled.p`
+    color: #222;
+    font-size: 16px;  
+    margin: 5px 0;
+`;
+
+const AdditionItemCircle = styled.span`
+    width: 15px;
+    height: 15px;
+    border-radius: 50%;
+    background-color: #70C294;
+    display: inline-block;
+    margin-right: 10px;
+`;
+
+const ServiceAddress = styled.h4`
+    font-size: 18px;
+    color: #222;
+    font-family: 'NotoSans-Bold';
+    margin: 10px 0;
+`;
+
+const ServiceMetro = styled.p`
+    font-size: 16px;
+    color: #222;  
+    margin: 0 0 10px 0;
+`;
+
+const MetroCircle = styled.span`
+    display: inline-block;
+    width: 15px;
+    height: 15px;
+    background-color: ${props=> props.backgroundColor};
+    margin-right: 5px;
+    border-radius: 50%;
+`;
+const WorkingHoursColumn = styled.span`
+    display: flex;
+    flex-direction: column;
+    margin-right: 20px;
+`;
+
+const WorkingHours = styled.span`
+    font-size: 14px;
+    margin: 2px 0 2px 0;
+`;
+
+const ServiceContactItem = styled.p`
+    font-size: 14px;
+    margin: 2px 0 2px 0;
+`;
+
+const ServiceContactImg = styled.img`
+    margin-right: 5px;
+`;
+
+const ServicePhoto = styled.img`
+    margin-left: 40px;
+    width: 300px;
+    height: 200px;
+`;
+
 
 
 function CompanyProfile(props) {
+    console.log('цены', props.address);
     return (
         <Wrapper>
             <Title>Мой профиль</Title>
             <Row>
-                <Column>
+                <MainProfile>
                     <Img src={`${props.contactPersonPhoto}`}/>
-                </Column>
-                <Column>
+                </MainProfile>
+                <MainProfile>
+                    <PersonLabel>Фамилия Имя Отчество</PersonLabel>
                     <PersonTitle>Смирнов Игорь Витальевич</PersonTitle>
                     <Row>
                         <PersonInfoBlock>
@@ -146,50 +286,53 @@ function CompanyProfile(props) {
                             <PersonInfo>{props.dateUpload}  {props.timeCreated}</PersonInfo>
                         </PersonInfoBlock>
                     </Row>
-                </Column>
-                <Button onClick={props.onEditCompanyProfile}>Редактировать профиль</Button>
+                </MainProfile>
+                <MainProfile>
+                    {/*<Button onClick={props.onEditCompanyProfile}>Редактировать профиль</Button>*/}
+                    <EditLink onClick={props.onEditCompanyProfile}><EditIcon icon={faPencilAlt} />Редактировать профиль</EditLink>
+                </MainProfile>
             </Row>
 
-            <Row marginTop={'20px'}>
-                <Column>
-                    <Title>Моя компания</Title>
+            <Row marginTop={'40px'}>
+                <Title>Моя компания <Status>не подтвержден</Status></Title> </Row>
+                <Row>
+                <Column margin={'0 10% 0 0'}>
+                    <CompanyLabel>Название компании</CompanyLabel>
                     <CompanyInfo>{props.companyName}</CompanyInfo>
-                    <CompanyInfo>ОГРН : {props.ogrn}</CompanyInfo>
-                    <CompanyInfo>ИНН : {props.inn}</CompanyInfo>
+                    <CompanyLabel>ОГРН</CompanyLabel>
+                    <CompanyInfo>{props.ogrn}</CompanyInfo>
+                    <CompanyLabel>ИНН</CompanyLabel>
+                    <CompanyInfo>{props.inn}</CompanyInfo>
                 </Column>
                 <Column>
-                    <Status>не подтвержден</Status>
+                    <AdnvantagesBlock backgroundImage={clipboard}>
+                        <AdnvantagesLabel>Выполненых заявок</AdnvantagesLabel>
+                        <AdvantagesCount>167</AdvantagesCount>
+                    </AdnvantagesBlock>
                 </Column>
-            </Row>
 
-            <Row marginTop={'20px'}>
-                <CompanyInfoBlock>
-                    <SubTitle>Адреса сервисов</SubTitle>
-                    <span>г. Москва, ул. Ленина дом 5 строение 2</span>
-                    <span>г. Москва, ул. Ленина дом 5 строение 2</span>
-                    <span>г. Москва, ул. Ленина дом 5 строение 2</span>
-                    <span>г. Москва, ул. Ленина дом 5 строение 2</span>
-                </CompanyInfoBlock>
-                <CompanyInfoBlock>
-                    <SubTitle>Режим работы</SubTitle>
-                    <span>пн-пт : 10:00 - 20:00, cб: 10:00 - 19:00</span>
-                    <span>пн-пт : 10:00 - 20:00, cб: 10:00 - 19:00</span>
-                    <span>пн-пт : 10:00 - 20:00, cб: 10:00 - 19:00</span>
-                    <span>пн-пт : 10:00 - 20:00, cб: 10:00 - 19:00</span>
-                </CompanyInfoBlock>
-                <CompanyInfoBlock>
-                    <SubTitle>Фото</SubTitle>
-                    <Img src={`${props.companyPhoto}`}/>
-                </CompanyInfoBlock>
-            </Row>
+                    <Column>
+                        <AdnvantagesBlock backgroundImage={review}>
+                            <AdnvantagesLabel>Оценка пользователей</AdnvantagesLabel>
+                            <AdvantagesCount>4.5/5</AdvantagesCount>
+                        </AdnvantagesBlock>
+                    </Column>
 
-            <Row marginTop={'20px'}>
-                <Column>
+                    <Column margin={'0 20px'}>
+                        <AdnvantagesBlock backgroundImage={star}>
+                            <AdnvantagesLabel>Рейтинг</AdnvantagesLabel>
+                            <AdvantagesCount>1895</AdvantagesCount>
+                        </AdnvantagesBlock>
+                    </Column>
+                </Row>
+
+            <Row marginTop={'40px'}>
+                <Column margin={'0 60px 0 0'}>
                     <SubTitle>Цены на ремонт</SubTitle>
-                <Price backgroundColor={'transparent'}>
-                    <PriceName>ремонт дисплея</PriceName>
-                    <PriceValue>5000 рублей</PriceValue>
-                </Price>
+                    <Price backgroundColor={'transparent'}>
+                        <PriceName>ремонт дисплея</PriceName>
+                        <PriceValue>5000 рублей</PriceValue>
+                    </Price>
 
                     <Price backgroundColor={'rgba(230,221,222,.2)'}>
                         <PriceName>ремонт дисплея</PriceName>
@@ -205,11 +348,75 @@ function CompanyProfile(props) {
                         <PriceName>ремонт дисплея</PriceName>
                         <PriceValue>5000 рублей</PriceValue>
                     </Price>
-
                 </Column>
 
+                <Column>
+                    <SubTitle>Дополнительные услуги</SubTitle>
+                    <AdditionItem><AdditionItemCircle/>Бесплатная диагностика</AdditionItem>
+                    <AdditionItem><AdditionItemCircle/>Доставка до сервиса</AdditionItem>
+                    <AdditionItem><AdditionItemCircle/>Работаем с юридическими лицами</AdditionItem>
+                </Column>
             </Row>
 
+
+            <Row marginTop={'40px'}>
+                <SubTitle>Адрес и режим работы</SubTitle> </Row>
+            <Row >
+                <Column>
+                    <ServiceAddress>г.Москва, ул. Соколово-Мещерская, д. 26 к.1</ServiceAddress>
+                    <ServiceMetro><MetroCircle backgroundColor={'#782678'}/>м. Планерная</ServiceMetro>
+                    <Row>
+                        <WorkingHoursColumn>
+                            <WorkingHours>Пн - 10:00 - 18:00</WorkingHours>
+                            <WorkingHours>Вт - 10:00 - 18:00</WorkingHours>
+                            <WorkingHours>Ср - 10:00 - 18:00</WorkingHours>
+                            <WorkingHours>Чт - 10:00 - 18:00</WorkingHours>
+                        </WorkingHoursColumn>
+
+                        <WorkingHoursColumn>
+                            <WorkingHours>Пт - 10:00 - 18:00</WorkingHours>
+                            <WorkingHours>Сб - 10:00 - 18:00</WorkingHours>
+                            <WorkingHours>Вск - выходной</WorkingHours>
+                        </WorkingHoursColumn>
+
+                        <WorkingHoursColumn>
+                            <ServiceContactItem><ServiceContactImg src={phone}/>+7(900)555-35-35</ServiceContactItem>
+                            <ServiceContactItem><ServiceContactImg src={phone}/>+7(900)555-35-35</ServiceContactItem>
+                        </WorkingHoursColumn>
+                    </Row>
+                </Column>
+                <ServicePhoto src={`${props.companyPhoto}`}/>
+            </Row>
+
+            {props.address.map((e, index)=>{
+                return (
+                    <Row marginTop={'40px'} key={index+e.street+e.house}>
+                        <Column>
+                            <ServiceAddress>{e.city}, {e.street}, {e.house}</ServiceAddress>
+                            <ServiceMetro><MetroCircle backgroundColor={'#D90102'}/>м. Чистые Пруды</ServiceMetro>
+                            <Row>
+                                <WorkingHoursColumn>
+                                    <WorkingHours>Пн - 10:00 - 18:00</WorkingHours>
+                                    <WorkingHours>Вт - 10:00 - 18:00</WorkingHours>
+                                    <WorkingHours>Ср - 10:00 - 18:00</WorkingHours>
+                                    <WorkingHours>Чт - 10:00 - 18:00</WorkingHours>
+                                </WorkingHoursColumn>
+
+                                <WorkingHoursColumn>
+                                    <WorkingHours>Пт - 10:00 - 18:00</WorkingHours>
+                                    <WorkingHours>Сб - 10:00 - 18:00</WorkingHours>
+                                    <WorkingHours>Вск - выходной</WorkingHours>
+                                </WorkingHoursColumn>
+
+                                <WorkingHoursColumn>
+                                    <ServiceContactItem><ServiceContactImg src={phone}/>8(800)555-35-35</ServiceContactItem>
+                                </WorkingHoursColumn>
+                            </Row>
+                        </Column>
+                        <ServicePhoto src={`${props.companyPhoto}`}/>
+                    </Row>
+                )
+            })}
         </Wrapper>
     )
 }
@@ -236,6 +443,7 @@ const mapStateToProps = (state) => {
         isPhoneNotification: state.company.isPhoneNotification,
         isEmailNews: state.company.isEmailNews,
         ogrn: state.company.ogrn,
+
     }
 }
 
