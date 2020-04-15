7718b9ccaff2a08e1255f28213968983f3be7b24
Igor <igor.salesmarketing@gmail.com>
2020-04-16 00:05:11 +0300

    Добавил базовый компонент Google карты

--- src/components/MapPage/MapPage.js
+++ src/components/MapPage/MapPage.js
@@ -3,15 +3,21 @@ import Navigation from "../Navigation/Navigation";
 import Navtop from "../Navtop/Navtop";
 import Footer from "../Footer/Footer";
 import MapComponent from "../Map/MapComponent";
+import NewMap from "../Map/NewMap";
+
+// google cloud platform
+
 
 const MapPage = () => {
     return (
         <React.Fragment>
             <Navigation/>
             <Navtop/>
-            <MapComponent/>
+            <NewMap/>
+            {/*<MapComponent/>*/}
             <Footer/>
         </React.Fragment>
     )
-}
+};
+
 export default MapPage
